734988bf203499102bcc2c9207a12106ef4dbec1
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2019-09-15 23:58:57 -0300

    Exercicios 49 completo e testado.

---
+++ MaratonaJava/src/exercicios/Ex49.java
@@ -0,0 +1,40 @@
+package exercicios;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Ex49 {
+
+	public static void main(String[] args) {
+		/*Crie um programa que apresente a soma e a média
+		 *  dos números armazenados em um arquivo em disco.*/
+		float soma  = 0;
+		float i = 0;
+		String linha = new String();
+		String nomeFile = "C:\\Users\\Samuel\\Documents\\Numeros.txt";
+		File file = new File(nomeFile);
+		
+		if(file.exists()) {
+			try {
+				FileReader leitor = new FileReader(nomeFile);
+				BufferedReader bufferFile = new BufferedReader(leitor);
+				
+				while(true) {
+					linha = bufferFile.readLine();
+					if(linha == null){
+						break;
+					}
+					soma += Float.parseFloat(linha);
+					i++;					
+				}
+				bufferFile.close();
+				Utils.Print("Soma dos numeros " + soma);
+				Utils.Print("Media dos numeros " + (soma / i));
+			}catch(Exception e){
+			}
+		}
+	}
+}
+
+
